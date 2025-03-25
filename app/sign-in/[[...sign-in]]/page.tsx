c90c8674c2a8389714893d447b3a47d2fb325285
Narayan Khanal <narayankhanal435@gmail.com>
2025-03-25 00:49:09 -0400

    add responsive height for signin route

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center my-20">
+    <div className="flex justify-center items-center sm:min-h-[85vh]">
       <SignIn />
     </div>
   );
